1cd440c8be5b2073eed27c697b956a38d8c6b776
hasan <hasanalmunawar07@gmail.com>
2025-07-09 10:33:30 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server( server, {
+        cors: {
+            origin: '*',
+            methods: ['GET', 'POST']
+        }
+})
+
+app.get('/', (req, res) => {
+    res.send("Server is running")
+})
+
+io.on('connection', (socket) => {
+    console.log("User Connected: ", socket.id);
+
+    socket.on('send-notification', (data) => {
+        io.emit('receive-notification', data)
+        console.log("receive-notification", data);
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+})
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
+
